7f6c656ef4f0f0f5e47a7ce9b5c527ac02ce77e5
Sanjida <sanjida.ice.778@gmail.com>
2025-03-11 19:41:08 +0600

    setup typescript with react

---
+++ src/router/router.tsx
@@ -0,0 +1,10 @@
+import { createBrowserRouter } from "react-router-dom";
+
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <h1 className="text-9xl">Mehefuj</h1>
+    }
+])
+
+export default router;
